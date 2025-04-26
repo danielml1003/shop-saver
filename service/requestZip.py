3047a9a74f3a4fedb3bf0226366ad35050a6ef14
danielml1003 <danielbaravik1003@gmail.com>
2025-04-26 12:20:38 +0300

    Update requestZip.py with new function and URL handling

--- service/requestZip.py
+++ service/requestZip.py
@@ -1,3 +1,21 @@
 import requests
 import os
+import datetime
 
+def job_that_runs_every_day():
+    now = datetime.datetime.now()
+    return now
+
+reshet  = {
+    "WStore": 1,
+    "WDate": job_that_runs_every_day(),
+    "WFileType":2
+}
+
+def makeUrl(date):
+    dataUrl = "https://kingstore.binaprojects.com/MainIO_Hok.aspx"
+    dataUrl += reshet.items
+    print(dataUrl)
+
+if __name__ == "__main__":
+    makeUrl(job_that_runs_every_day())
